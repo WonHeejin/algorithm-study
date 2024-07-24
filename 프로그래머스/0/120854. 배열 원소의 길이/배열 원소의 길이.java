590933c76122894ee53f747331f11d5c1a3c2ba6
WonHeejin <94436793+WonHeejin@users.noreply.github.com>
2024-07-24 11:57:00 +0900

    [level 0] Title: 배열 원소의 길이, Time: 3.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,9 +1,12 @@
+/*배열에 담긴 데이터 모두에 동일한 작업 반복
+ -> stream 활용하는 방법으로 수정*/
+import java.util.stream.*;
+import java.util.Arrays;
 class Solution {
     public int[] solution(String[] strlist) {
-        int[] answer = new int[strlist.length];
-        for(int i=0; i<strlist.length; i++){
-            answer[i] = strlist[i].length();
-        }
+        int[] answer = Arrays.stream(strlist) //배열을 stream으로 변환
+            .mapToInt(String::length) // String.length 함수에 배열 요소 담은 후 int로 변환
+            .toArray(); //배열로 변환
         return answer;
     }
 }
