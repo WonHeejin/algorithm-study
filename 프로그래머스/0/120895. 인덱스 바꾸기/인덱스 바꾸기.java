0972ea2fe65842902d8dd4c54dc851c2e87da609
WonHeejin <94436793+WonHeejin@users.noreply.github.com>
2024-07-29 09:41:29 +0900

    Update 인덱스 바꾸기.java - 주석 추가
    StringBuilder의 setCharAt에 대한 설명 추가

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -5,6 +5,7 @@ class Solution {
         char char1 = my_string.charAt(num1);
         char char2 = my_string.charAt(num2);
         StringBuilder builder = new StringBuilder(my_string);
+        //setCharAt(int index, char ch) : index번째 문자를 ch로 변경
         builder.setCharAt(num1, char2);
         builder.setCharAt(num2, char1);
         answer = builder.toString();
