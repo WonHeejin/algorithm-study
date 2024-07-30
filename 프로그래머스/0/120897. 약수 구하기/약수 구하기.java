c2456f48e4cadecc1d9ccc6b2305848baa5abab2
WonHeejin <94436793+WonHeejin@users.noreply.github.com>
2024-07-30 13:14:47 +0900

    [level 0] Title: 약수 구하기, Time: 6.83 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,10 @@
+import java.util.stream.IntStream;
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = IntStream.rangeClosed(1, n)
+            .filter(i->n%i==0)
+            .sorted()
+            .toArray();
+        return answer;
+    }
+}
