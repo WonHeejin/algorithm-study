1be51c0ce66116023b1176aeed4b523cd030ea05
WonHeejin <94436793+WonHeejin@users.noreply.github.com>
2024-07-30 13:28:00 +0900

    [level 0] Title: 약수 구하기, Time: 3.58 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/README.md
+++ 프로그래머스/0/120897. 약수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 약수 구하기 - 120897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120897?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120897) 
 
 ### 성능 요약
 
-메모리: 80.8 MB, 시간: 6.83 ms
+메모리: 79.8 MB, 시간: 3.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 30일 13:14:46
+2024년 07월 30일 13:27:59
 
 ### 문제 설명
 
--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,10 +1,20 @@
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 class Solution {
     public int[] solution(int n) {
-        int[] answer = IntStream.rangeClosed(1, n)
-            .filter(i->n%i==0)
-            .sorted()
-            .toArray();
+        List<Integer> list = new ArrayList<Integer>();
+        //약수는 대칭을 이루기 때문에 제곱근 만큼만 반복
+        for(int i=1; i<=Math.sqrt(n); i++) {
+            if(n%i == 0) {
+                list.add(i);
+                if(n/i!=i) list.add(n/i); //같은 수가 중복되지 않도록 예외처리
+            }
+        }
+        //list -> array 변환
+        int[] answer = list.stream().mapToInt(e->e).toArray();
+        //오름차순 정렬
+        Arrays.sort(answer);
         return answer;
     }
 }
