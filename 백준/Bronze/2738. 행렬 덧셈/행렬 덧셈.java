d80eb1dabc94fcf9581764bfee4c152d71b29667
WonHeejin <94436793+WonHeejin@users.noreply.github.com>
2024-08-12 10:59:40 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 172 ms, Memory: 17848 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,35 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        StringBuffer sb = new StringBuffer();
         int[][] a = new int[n][m];
-        int[][] b = new int[n][m];
         
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                a[i][j] = sc.nextInt();
+        for(int i=0; i<2; i++) { //두 개의 행렬을 받음
+            for(int j=0; j<n; j++) {
+                st = new StringTokenizer(br.readLine());
+                for(int k=0; k<m; k++) {
+                    a[j][k] += Integer.parseInt(st.nextToken());
+                    if(i==1) sb.append(a[j][k] + " ");
+                }
+                if(i==1) sb.append("\n");
             }
         }
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                b[i][j] = sc.nextInt();
-            }
-        }
-        solution(n,m,a,b);
-    }
-    
-    public static void solution(int n, int m, int[][] a, int[][] b) {
-        int[][] answer = new int[n][m];
         
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                answer[i][j] = a[i][j] + b[i][j];
-                System.out.print(answer[i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.print(sb.toString());
     }
 }
