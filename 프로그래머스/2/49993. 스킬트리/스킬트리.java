b81bd91448c7faa474889d1180c3f6657ae7dabe
WonHeejin <94436793+WonHeejin@users.noreply.github.com>
2024-09-02 13:27:45 +0900

    [level 2] Title: 스킬트리, Time: 15.40 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,24 +1,12 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-        int index = 0;
-        for(int j=0; j<skill_trees.length; j++) {
-            for (int i=0; i<skill_trees[j].length(); i++){
-                //스킬 순서에 존재하면 검사
-                if(skill.indexOf(skill_trees[j].charAt(i)) != -1) {
-                    if(skill.charAt(index) == skill_trees[j].charAt(i)) {
-                        index++; //다음 문자 검사
-                    } else {
-                        break; //skill_trees[j] 검사 종료. 다음 검사로 넘어감.
-                    }
-                }
-                if(i == skill_trees[j].length()-1) { //가능한 스킬트리일 경우
-                    answer ++;
-                }   
-            }    
-            index = 0;
+        int answer = skill_trees.length;
+        for(int i=0; i<skill_trees.length; i++) {
+            //skill_trees의 문자열에서 skill을 제외한 문자 모두 제거 -> indexOf로 skill문자와 비교
+            if(skill.indexOf(skill_trees[i].replaceAll("[^"+skill+"]",""))!=0) {
+                answer--; //스킬트리 불가능한 경우 제외
+            }
         }
-        
         return answer;
     }
 }
