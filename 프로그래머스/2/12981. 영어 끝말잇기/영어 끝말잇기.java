f46b7e561b5db586a47fc2fbedb8dcdff601fd81
WonHeejin <94436793+WonHeejin@users.noreply.github.com>
2024-09-04 11:03:57 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.13 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,23 +1,22 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.HashSet;
 
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        Deque<String> deque = new ArrayDeque<>();
-        deque.add(words[0]);
+        HashSet<String> history = new HashSet<>(); //중복값 허용 안함
+        char lastChar = words[0].charAt(words[0].length()-1);
+        history.add(words[0]);
         
         for(int i=1; i<words.length; i++) {
-            String latest = deque.getLast();
+            history.add(words[i]);
             //앞 단어의 마지막 문자와 다른 경우 || 기존에 언급된 경우
-            if(latest.charAt(latest.length()-1) != words[i].charAt(0)
-              || deque.contains(words[i])) {
-                i++;
-                answer[0] = i%n == 0? n : i%n;
-                answer[1] = i%n == 0? i/n : i/n+1;
+            if(lastChar != words[i].charAt(0)
+              || history.size() != i+1) {
+                answer[0] = i%n+1;
+                answer[1] = i/n+1;
                 break;
             }
-            deque.addLast(words[i]);
+            lastChar = words[i].charAt(words[i].length()-1);
         }
         
         return answer;
