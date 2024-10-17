7259368ffd1a0420521569bc8ed96348c9aab928
WonHeejin <94436793+WonHeejin@users.noreply.github.com>
2024-10-17 11:03:39 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,13 +1,10 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int sqrt = (int) Math.floor(Math.sqrt(n));
-        if(n%sqrt == 0) {
-            answer = 1;
+        if(n%Math.sqrt(n) == 0) {
+            return 1;
         } else {
-            answer = 2;
+            return 2;
         }
         
-        return answer;
     }
 }
