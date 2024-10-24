3da040f46fc3645903875f214b44b0263d742c26
WonHeejin <94436793+WonHeejin@users.noreply.github.com>
2024-10-24 11:42:22 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,18 +2,8 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int ans = 0;
         //도착지로부터 계산 -> 짝수일 땐 순간이동, 홀수일 땐 1칸 점프
-        while(n>2) {
-            if(n%2==1) {
-                n-=1;
-                ans++;
-            }
-                n/=2;
-        }
-        //n==2 -> 1칸 앞으로 점프 후 순간이동 -> ans++;
-        ans++;
-
-        return ans;
+        //bitCount : 주어진 정수를 2진법으로 표현했을 때 1의 갯수 반환
+        return Integer.bitCount(n);
     }
 }
